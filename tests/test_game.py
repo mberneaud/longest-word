2c73f7a4d6e359ede04b230eb6a3027e356fd560
Malte Berneaud <malte.berneaud@gmail.com>
2023-10-24 19:12:35 +0000

    feature complete: check against API if word is english word

--- tests/test_game.py
+++ tests/test_game.py
@@ -25,6 +25,11 @@ class TestGame:
         new_game = Game() # initiates grid to ['U', 'D', 'A', 'X', 'I', 'H', 'H', 'E', 'X']
         assert new_game.is_valid("DAISY") == False
 
+    def test_unkown_word2_is_invalid(self):
+        random.seed(42)
+        new_game = Game() # initiates grid to ['U', 'D', 'A', 'X', 'I', 'H', 'H', 'E', 'X']
+        assert new_game.is_valid("DAXI") == False
+
     def test_unknown_word_is_invalid(self):
         """A word that is not in the english directory should no be valid"""
         new_game = Game()
